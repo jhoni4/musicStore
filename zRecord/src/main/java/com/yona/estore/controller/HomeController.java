f495dd34abf505991d3786d13940aab6743c8f2a
yonatan molla <jhoni42002@yahoo.com>
2017-02-12 23:30:49 -0600

    admin controller added

--- zRecord/src/main/java/com/yona/estore/controller/HomeController.java
+++ zRecord/src/main/java/com/yona/estore/controller/HomeController.java
@@ -1,31 +1,33 @@
 package com.yona.estore.controller;
 
-
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.yona.estore.dao.ProductDao;
-import com.yona.estore.model.Product;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @Autowired
-    private ProductDao productDao;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
 
-    @RequestMapping("/")
-    public String home() {
-        return "home";
-    }
+	@RequestMapping("/login")
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model) {
+		if (error != null) {
+			model.addAttribute("error", "Invalid username and password");
+		}
 
+		if (logout != null) {
+			model.addAttribute("msg", "You have been logged out successfully.");
+		}
 
-   
+		return "login";
+	}
+
 }
---
+++ zRecord/src/main/java/com/yona/estore/controller/admin/AdminHomeController.java
@@ -0,0 +1,41 @@
+package com.yona.estore.controller.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yona.estore.model.Product;
+import com.yona.estore.service.ProductService;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminHomeController {
+
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping
+    public String adminPage() {
+        return "admin";
+    }
+
+    @RequestMapping("/productInventory")
+    public String productInventory(Model model) {
+        List<Product> products = productService.getProductList();
+        model.addAttribute("products", products);
+
+        return "productInventory";
+    }
+
+    @RequestMapping("/customer")
+    public String customerManagement(Model model) {
+
+        //to add some customer service later
+        return "customerManagement";
+    }
+}
